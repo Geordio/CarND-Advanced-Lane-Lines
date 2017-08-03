c371194ae43a8daea69a1d1fc3d1315f38c27b3f
George Haigh <georgehaigh@hotmail.com>
2017-08-03 22:14:01 +0100

    initial commit

---
+++ util.py
@@ -0,0 +1,111 @@
+import cv2
+import numpy as np
+
+def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+    # Apply the following steps to img
+    # 1) Convert to grayscale
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    # 3) Take the absolute value of the derivative or gradient
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    # 5) Create a mask of 1's where the scaled gradient magnitude
+    # is > thresh_min and < thresh_max
+    # 6) Return this mask as your binary_output image
+    # binary_output = np.copy(img) # Remove this line
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    else:
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    # absolute value
+    abs_sobel = np.absolute(sobel)
+    # scale
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+
+    # thresh_min = 20
+    # thresh_max = 100
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+    # plt.imshow(sxbinary, cmap='gray')
+    binary_output = sbinary
+    return binary_output
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+
+
+def hls_select(img, thresh=(0, 255)):
+    # 1) Convert to HLS color space
+    # 2) Apply a threshold to the S channel
+    # 3) Return a binary image of threshold result
+
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    H = hls[:, :, 0]
+    L = hls[:, :, 1]
+    S = hls[:, :, 2]
+    # thresh = (90, 255)
+    binary_output = np.zeros_like(S)
+    binary_output[(S > thresh[0]) & (S <= thresh[1])] = 1
+    return binary_output
+
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('bridge_shadow.jpg')
+
+
+# Edit this function to create your own pipeline.
+def pipeline(img, s_thresh=(170, 230), sx_thresh=(30, 100)):
+    img = np.copy(img)
+    # Convert to HSV color space and separate the V channel
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hsv[:, :, 1]
+    s_channel = hsv[:, :, 2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack each channel
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
+    # be beneficial to replace this channel with something else.
+    color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary))
+    return color_binary
+
+
+result = pipeline(image)
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=40)
+
+ax2.imshow(result)
+ax2.set_title('Pipeline Result', fontsize=40)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
